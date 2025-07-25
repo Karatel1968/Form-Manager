8655e659500bb2032fdb6cc823d8879269efd830
Oleg Urin <olegurin76@mail.ru>
2025-07-25 17:37:07 +0300

    начал писать форму, добавил ещё зависимостей

--- form-manager/src/pages/UserCreate/FormikForm.tsx
+++ form-manager/src/pages/UserCreate/FormikForm.tsx
@@ -1,1 +1,25 @@
 import { Formik, Form, Field } from 'formik';
+import { userSchema } from '../../shared/lib/validationSchemas';
+
+export const FormikForm = ({ onSubmit }: { onSubmit: (values: any) => void }) => {
+  return (
+    <Formik
+      initialValues={{
+        name: '',
+        surName: '',
+        password: '',
+        confirmPassword: '',
+        fullName: '',
+        email: '',
+        birthDate: '',
+        telephone: '',
+        employment: '',
+        userAgreement: false
+      }}
+      validationSchema={userSchema}
+      onSubmit={onSubmit}
+    >
+
+    </Formik>
+  );
+}
